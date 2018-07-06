2369e75686ef3b36757bb78fe3cad54e10b48588
Ben Robinson <mentionbenjamin@gmail.com>
2018-07-06 13:17:53 +0100

    InSetup and tests passing for Drums

--- src/main/java/Instruments/Percussion/Drums.java
+++ src/main/java/Instruments/Percussion/Drums.java
@@ -1,4 +1,11 @@
 package Instruments.Percussion;
 
-public class Drums {
+import Instruments.Instrument;
+
+public class Drums extends Instrument {
+
+    public Drums(String name, String family, int costPrice, int sellPrice){
+        super(name, family, costPrice, sellPrice);
+    }
+
 }
--- src/test/java/Instruments/Percussion/DrumsTest.java
+++ src/test/java/Instruments/Percussion/DrumsTest.java
@@ -1,4 +1,22 @@
 package Instruments.Percussion;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrumsTest {
+
+    Drums drums;
+
+    @Before
+    public void setUp(){
+        drums = new Drums("Drums E110", "Percussion", 100, 200);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Drums E110", drums.getName());
+    }
+
 }
